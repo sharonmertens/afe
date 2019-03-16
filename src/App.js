019e9b08ba92be1d3fc318bd4b167e4b4af0da3c
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-15 21:09:13 -0400

    fetch goals created to localhost

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import GoalList from './components/GoalList'
 import Form from './components/Form'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      goals: []
+    }
+  }
+
+  // fetching our api data
+  fetchGoals = () => {
+    console.log('fetch goals is running')
+    fetch('http://localhost:3000/goals')
+    .then(data => data.json())
+    .then(jData => {
+      this.setState({ goals: jData })
+    })
+  }
+
   render() {
+    // this.fetchGoals()
     return (
       <div className="main-container">
         <h1>Accountability Frontend</h1>
