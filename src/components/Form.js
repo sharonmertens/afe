b156647157f05f22aaf6d142a0f35f769f11df4c
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-17 11:53:30 -0400

    added form to submit new recipe

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      description: '',
+      ingredients: ''
+    }
+  }
+
   render () {
     return (
       <div className="form">
-        this is the form component
+        <form>
+          <input
+            type="text"
+            placeholder="description"
+            id="description"
+          />
+          <br/>
+          <input
+            type="text"
+            placeholder="ingredients"
+            id="ingredients"
+          />
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,8 +4,8 @@ class Recipe extends Component {
   render () {
     return (
       <div className="recipe">
-        <h3>{this.props.recipe.description}</h3>
-        <h3>{this.props.recipe.ingredients}</h3>
+        <h3>Description: {this.props.recipe.description}</h3>
+        <h3>Ingredients: {this.props.recipe.ingredients}</h3>
         <br/>
       </div>
     )
