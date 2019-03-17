f7598b18aafc9fae5606dfe8dc110b87b98529d7
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-03-17 12:01:45 -0400

    functionality for creating a new recipe

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,20 +9,45 @@ class Form extends Component {
     }
   }
 
+  // handle change
+  handleChange = (event) => {
+    // console.log(event.target.value)
+    this.setState({ [event.target.id]: event.target.value})
+  }
+
+  // handle submit
+  handleSubmit = (event) => {
+      event.preventDefault()
+      console.log(this.state)
+      this.clearForm()
+  }
+
+  // clear the form
+  clearForm = () => {
+    this.setState({
+      description: '',
+      ingredients: ''
+    })
+  }
+
   render () {
     return (
       <div className="form">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="description"
             id="description"
+            value={this.state.description}
+            onChange={this.handleChange}
           />
           <br/>
           <input
             type="text"
             placeholder="ingredients"
             id="ingredients"
+            value={this.state.ingredients}
+            onChange={this.handleChange}
           />
           <br/>
           <button type="submit">Submit</button>
